16d24bd67bc08eb4f180cea859aac65b1ddb59c7
davidvel25 <davidvel25@outlook.com>
2022-05-02 13:33:00 -0700

    finished (add comments)
    fixed all the bugs in my code

--- final/script.js
+++ final/script.js
@@ -1,12 +1,10 @@
 // define a Constructor function
-var Food = function(fName, fCal) {
-    this.name = parseInt(fName);
-    this.cal = parseInt(fCal);
+var Food = function(pName, pCal) {
+    this.name = pName;
+    this.cal = parseInt(pCal);
 };
 
 FoodArray = []; // define an array to hold Customer objects
-
-let calorieCount = 0;
 
 // pre-load array with 2 objects
 let newFood1 = new Food("Banana", 20);
@@ -17,26 +15,37 @@ FoodArray.push(newFood2);
 
 function submitForm(){
     //derive the vlaues from the textboxes to be placed in array and outputted to console
-    let fName = document.getElementById("foodName").value;
-    let fCal = document.getElementById("calorieNumber").value;
-    FoodArray.push(new Food(fName, fCal));
+    let vName = document.getElementById("foodName").value;
+    let vCal = document.getElementById("calorieNumber").value;
+    console.log("submit form called");
+    console.log(vName);
+    console.log(vCal);
+    FoodArray.push(new Food(vName, vCal));
+}
 
-    //clear text boxes for new entry
-    document.getElementById("foodName").value = "";
-    document.getElementById("calorieNumber").value = "";
+function clearFields(){
+        //clear text boxes for new entry
+        document.getElementById("foodName").value = "";
+        document.getElementById("calorieNumber").value = "";
+        console.log(FoodArray);
+}
+
+function calculateTotal(){
+    let calorieCount = 0;
+    for (i = 0; i < FoodArray.length; i++) {
+        calorieCount += FoodArray[i].cal;
+        console.log(FoodArray[i]);
+        console.log(calorieCount);
+    }
+    document.getElementById("calorieTotal").value = calorieCount;
 }
 
 document.addEventListener("DOMContentLoaded", function (){
 
     document.getElementById("submit").addEventListener("click", function(){
         console.log(FoodArray);
+        submitForm();
+        calculateTotal();
+        clearFields();
     });
-
-    for (i = 0; i < FoodArray.length; i++) {
-            calorieCount += FoodArray[i].fCal;
-            console.log(FoodArray[i]);
-        }
 });
-
-document.getElementById("calorieTotal").value = calorieCount;
-
