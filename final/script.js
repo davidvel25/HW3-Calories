150ff06b477d7db9c0c38c67ad9c9f0d72e4fbdd
davidvel25 <davidvel25@outlook.com>
2022-05-02 12:58:50 -0700

    debug

--- final/script.js
+++ final/script.js
@@ -1,17 +1,30 @@
 // define a Constructor function
 var Food = function(fName, fCal) {
-    this.name = fName;
-    this.cal = fCal;
+    this.name = parseInt(fName);
+    this.cal = parseInt(fCal);
 };
 
 FoodArray = []; // define an array to hold Customer objects
 
+let calorieCount = 0;
+
 // pre-load array with 2 objects
-let newFood1 = new Food("Banana",  20);
+let newFood1 = new Food("Banana", 20);
 FoodArray.push(newFood1);
 
-let newFood2 = new Food("Hot Dog",  450);
+let newFood2 = new Food("Hot Dog", 450);
 FoodArray.push(newFood2);
+
+function submitForm(){
+    //derive the vlaues from the textboxes to be placed in array and outputted to console
+    let fName = document.getElementById("foodName").value;
+    let fCal = document.getElementById("calorieNumber").value;
+    FoodArray.push(new Food(fName, fCal));
+
+    //clear text boxes for new entry
+    document.getElementById("foodName").value = "";
+    document.getElementById("calorieNumber").value = "";
+}
 
 document.addEventListener("DOMContentLoaded", function (){
 
@@ -20,17 +33,10 @@ document.addEventListener("DOMContentLoaded", function (){
     });
 
     for (i = 0; i < FoodArray.length; i++) {
+            calorieCount += FoodArray[i].fCal;
             console.log(FoodArray[i]);
         }
 });
 
-function submitForm(){
-    //derive the vlaues from the textboxes to be placed in array and outputted to console
-    let fName = document.getElementById("foodName").value;
-    let fCal = document.getElementById("calorieNumber").value;
-    FoodArray.push(new Customer(fName, fCal));
+document.getElementById("calorieTotal").value = calorieCount;
 
-    //clear text boxes for new entry
-    document.getElementById("foodName").value = "";
-    document.getElementById("calorieNumber").value = "";
-}
